ecdc2e3518843cef9a25aa3532cddd742940d4d7
RohitShah1706 <rlshah03@gmail.com>
2022-09-17 15:30:56 +0530

    footer done - logo pending

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,64 @@
 import "./Footer.scss";
+import { AiFillInstagram } from "react-icons/ai";
+import { BsFacebook, BsLinkedin } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+import { isMobile } from "react-device-detect";
 const Footer = () => {
     return (
         <>
             <div className="row">
                 <div className="col-sm">
                     <div className="footer-content">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quas velit, quod in repudiandae incidunt et dicta! Libero ipsum earum nisi beatae soluta sequi expedita atque! Voluptate, numquam impedit! Temporibus mollitia sequi voluptas quaerat, in commodi repellendus suscipit culpa? Quod quo modi iste laborum ipsum consectetur numquam quisquam dolores aspernatur rerum asperiores debitis ratione nam, odio rem amet, maiores, est quis itaque cumque quaerat culpa praesentium corporis architecto. Sit dolore quas laboriosam. In, laborum ad. Deserunt beatae aut quos eaque, quaerat quam consectetur facilis veniam molestiae ullam recusandae dolores, sint officiis inventore sequi earum quidem a. Quia dolor a omnis!
                         <div className="row">
+                            {/* bootstrap class to center in col */}
                             <div className="col-sm">
-                                <h1>Logo</h1>
-                            </div>
-                            <div className="col-sm">
-                                <h1>IIITB- logo</h1>
+                                <span>
+                                    <h1 className="footer-logo">Logo</h1>
+                                </span>
+                                <span>
+                                    <h1 className="footer-logo">IIITB-logo</h1>
+                                </span>
+                                <p className="footer-c">© Synergy 2023, IIIT Bangalore</p>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* ! mobile content not shown in mobile view */}
-                <div className="col-sm mobile">
-                    <div className="footer-content">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quas velit, quod in repudiandae incidunt et dicta! Libero ipsum earum nisi beatae soluta sequi expedita atque! Voluptate, numquam impedit! Temporibus mollitia sequi voluptas quaerat, in commodi repellendus suscipit culpa? Quod quo modi iste laborum ipsum consectetur numquam quisquam dolores aspernatur rerum asperiores debitis ratione nam, odio rem amet, maiores, est quis itaque cumque quaerat culpa praesentium corporis architecto. Sit dolore quas laboriosam. In, laborum ad. Deserunt beatae aut quos eaque, quaerat quam consectetur facilis veniam molestiae ullam recusandae dolores, sint officiis inventore sequi earum quidem a. Quia dolor a omnis!
+                {!isMobile &&
+                    <div className="col-sm mobile">
+                        <div className="footer-content">
+                            <div className="row">
+                                <div className="col-sm">
+                                    <h1 className="footer-heading">Social</h1>
+                                    <h1 className="footer-subheading">Handles</h1>
+                                </div>
+                                <div className="col-sm">
+                                    <div className="row">
+                                        {/* space items evenly in column */}
+                                        <div className="col-sm foot-icon-list">
+                                            <a href="https://www.instagram.com/iiitb_official/" target="_blank" rel="noreferrer" className="footer-icon">
+                                                <AiFillInstagram style={{ color: "white", fontSize: "2.5em" }} />
+                                            </a>
+                                            <a href="https://www.facebook.com/IIITBofficial/" target="_blank" rel="noreferrer" className="footer-icon">
+                                                <BsFacebook style={{ color: "white", fontSize: "2.3em" }} />
+                                            </a>
+                                            <a href="https://in.linkedin.com/school/iiit-bangalore/" target="_blank" rel="noreferrer" className="footer-icon">
+                                                <BsLinkedin style={{ color: "white", fontSize: "2.3em" }} />
+                                            </a>
+                                            <a href="https://twitter.com/IIITB_official" target="_blank" rel="noreferrer" className="footer-icon">
+                                                <FaTwitter style={{ color: "white", fontSize: "2.3em" }} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="row handles-sub">
+                                        Connect and Don't miss a thing.
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                }
             </div>
         </>
     )
