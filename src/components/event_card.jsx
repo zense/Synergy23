855c840dc81d31c140f18e9756380f78eb9e4c43
RohitShah1706 <rlshah03@gmail.com>
2023-10-05 00:00:50 +0530

    updated event link

--- src/components/event_card.jsx
+++ src/components/event_card.jsx
@@ -26,7 +26,7 @@ function EventCard(props) {
                     <div className="event_text">{props.description1}</div>
                 </div>
                 <div className='accordion-content'>
-                    <div className="event_text"><a href="https://iiitbac-my.sharepoint.com/personal/synergy_iiitb_ac_in/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fsynergy_iiitb_ac_in%2FDocuments%2FSynergy '23%2FEvents%2FLens Flare%2FLens Flare.pdf&parent=%2Fpersonal%2Fsynergy_iiitb_ac_in%2FDocuments%2FSynergy '23%2FEvents%2FLens Flare&ga=1" target="_blank">Event Details    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+                    <div className="event_text"><a href="https://iiitbac-my.sharepoint.com/:b:/g/personal/synergy_iiitb_ac_in/Ea5FWwrd4u1MvqB2XHkr6zcBnflgZbikeUXe8kRi8H_VbA?e=MRTaCf" target="_blank">Event Details    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
   <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
   <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
 </svg></a></div>
