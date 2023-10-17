d8183f09f0a21e0df0309c0fc76e75e5cc9caa72
RohitShah1706 <rlshah03@gmail.com>
2023-10-17 14:56:34 +0530

    changed event link for pitching pixels

--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -72,9 +72,9 @@ function Events() {
               <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z" />
               <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z" />
             </svg>}
-        btn1_text="Register"
+        btn1_text="Register on Unstop"
         link1={
-          "https://docs.google.com/forms/d/e/1FAIpQLSeuJr_17cQXBrO7boOP-BOPO_U5NGrtFBSR2swA-5O87HT9fQ/viewform?usp=sf_link"
+          "https://unstop.com/o/tlpTsFa"
         }
         submit={""}
       />
